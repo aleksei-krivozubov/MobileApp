47c975ca3889df0669ed9c22fa0a438ee1e10c76
aleksei-krivozubov <krivozubov2011@mail.ru>
2018-04-17 08:27:52 +0300

    Добавлены файлы страниц приложения

---
+++ AuthPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Proga
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthPage : ContentPage
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+           
+        }
+        private void switcher_Toggled(object sender, ToggledEventArgs e)
+        {
+            passwordEntry.IsPassword = !(switcher.IsToggled);
+        }
+
+        private void Reg_user(object sender, System.EventArgs e)
+        {
+            //await Navigation.PushAsync(new Registration());
+        }
+
+    }
+}
